ace475d87f7e9bf85f5f8cbde3686f271b02d2c6
Vincent <uz18855@bristol.ac.uk>
2021-08-02 10:42:19 +0100

    stratified shuffle split for data pairs

--- oneinamillion/pc_consultation.py
+++ oneinamillion/pc_consultation.py
@@ -3,12 +3,11 @@ from dataclasses import dataclass
 from typing import List
 
 import numpy as np
+from sklearn.model_selection import StratifiedShuffleSplit
 
 from .primary_care.patient_record import PatientRecordParser, PatientRecords
 from .primary_care.record_code import RecordCodeParser
 from .primary_care.transcript import TranscriptParser, Transcript
-
-from sklearn.model_selection import StratifiedShuffleSplit
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -26,23 +25,26 @@ class PCConsultation:
     record_code_parser = RecordCodeParser()
 
     doc_ids: list = []
+    _seed = 0
 
-    def __init__(self):
+    def __init__(self, seed=None):
         self._get_doc_ids()
+        if seed:
+            self._seed = seed
 
-    def get_from_ids(self, record_ids: List[str]) -> [PrimaryCareDataPair]:
+    def get_sequence(self, record_ids: List[str]) -> [PrimaryCareDataPair]:
         for record_id in record_ids:
-            yield self.get(record_id)
+            yield self.get_single(record_id)
 
-    def get(self, record_id: str) -> PrimaryCareDataPair:
+    def get_single(self, record_id: str) -> PrimaryCareDataPair:
         codes = self.record_code_parser.get(record_id)
         pt_record = self.record_parser.get(record_id)
         transcript = self.transcript_parser.get(record_id)
         return PrimaryCareDataPair(codes, pt_record, transcript)
 
     def _get_doc_ids(self):
-        pt_record_list = self.record_parser.get_doc_ids()
-        transcript_list = self.transcript_parser.get_doc_ids()
+        pt_record_list = self.record_parser.doc_ids
+        transcript_list = self.transcript_parser.doc_ids
         mutual_ids = set(pt_record_list).intersection(transcript_list)
         self.doc_ids = list(mutual_ids)
 
@@ -54,31 +56,25 @@ class PCConsultation:
         logging.warning(f"The current IDs only have transcript documents.\n{np.sort(np.array(list(only_transcript)))}")
 
     # return list of indexes for train and test sets
-    def create_train_test_split(self, n_splits=1):
+    def create_train_test_split(self, n_splits=1, test_size=0.2):
         codes = [self.record_code_parser.get(doc) for doc in self.doc_ids]
-        ll = []
+        flattened_pairs = []
         for _id, _codes in zip(self.doc_ids, codes):
             if type(_codes) == list:
                 for c in _codes:
-                    ll.append((_id, c))
+                    flattened_pairs.append((_id, c))
             else:
-                ll.append((_id, _codes))
-
-        doc_ids, codes = zip(*ll)
-        category_codes = (lambda xs: [x[0] for x in xs])(codes)
-
-        stratified_shuffle = StratifiedShuffleSplit(n_splits=n_splits, test_size=0.2, random_state=0)
-        doc_ids = np.array(doc_ids).reshape(-1, 1)
-        category_codes = np.array(category_codes).reshape(-1, 1)
-        splits = stratified_shuffle.split(doc_ids, category_codes)
-
-        for train_split, test_split in splits:
-            def _iloc(xs):
-                return [doc_ids[x][0] for x in xs]
-
-            yield _iloc(train_split), _iloc(test_split)
+                flattened_pairs.append((_id, _codes))
+        doc_ids, codes = zip(*flattened_pairs)
+        n_samples = len(doc_ids)
+        doc_ids = np.array(doc_ids).reshape((n_samples, -1))
+        classified_codes = [c[0] for c in codes]
+        classified_codes = np.array(classified_codes).reshape((n_samples,))
+        split = StratifiedShuffleSplit(n_splits=n_splits, test_size=test_size, random_state=self._seed)
+        for train_index, test_index in split.split(doc_ids, classified_codes):
+            yield train_index, test_index
 
 
 if __name__ == '__main__':
     pc = PCConsultation()
-    print("test")
+
