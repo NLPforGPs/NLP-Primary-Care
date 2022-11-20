220d734fd86a7bba4a46d804efce4bb7a431c60e
Edwin Simpson <edwin.simpson@gmail.com>
2022-11-20 22:43:59 +0000

    updates to scripts

--- scripts/distant_supervision.sh
+++ scripts/distant_supervision.sh
@@ -18,7 +18,7 @@ module add lang/python/anaconda/pytorch
 # module add lang/python/anaconda/3.8-2020.07
 
 #conda env create --file nlp_gp.yml
-#eval "$(conda shell.bash hook)"
+eval "$(conda shell.bash hook)"
 conda activate NLP_GP
 
 echo Start Time: $(date)
