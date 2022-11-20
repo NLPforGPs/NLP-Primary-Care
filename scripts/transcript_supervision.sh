a0046d8cdc50582a12daebb3096366d043481ef4
Edwin Simpson <edwin.simpson@gmail.com>
2022-11-20 22:39:34 +0000

    updates to scripts

--- scripts/transcript_supervision.sh
+++ scripts/transcript_supervision.sh
@@ -17,10 +17,10 @@ echo Working Directory: $(pwd)
 module add lang/python/anaconda/pytorch
 # module add lang/python/anaconda/3.8-2020.07
 
-conda env create --file nlp_gp.yml
-eval "$(conda shell.bash hook)"
+#conda env create --file nlp_gp.yml
+#eval "$(conda shell.bash hook)"
 conda activate NLP_GP
 
 echo Start Time: $(date)
-time python3 ./run_transcript_supervision.py
+time run -n NLP_GP --no-capture-output python -u ./run_transcript_supervision.py
 echo End Time: $(date)
