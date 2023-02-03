58368feaf2568d8e41c6936a09d9edf19eda59ca
Edwin Simpson <edwin.simpson@gmail.com>
2023-02-03 18:28:39 +0000

    fix changes in the slurm scripts

--- scripts/distant_supervision.sh
+++ scripts/distant_supervision.sh
@@ -25,5 +25,5 @@ module add lang/python/anaconda/pytorch
 #conda install datasets --yes
 
 echo Start Time: $(date)
-conda run -n NLP_GP --no-capture-output python -u ./run_distant_supervision.py --experiments_to_run=1234
+conda run -n NLP_GP --no-capture-output "python -u ./run_distant_supervision.py --experiments_to_run=1234"
 echo End Time: $(date)
